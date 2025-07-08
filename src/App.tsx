85c0d6226ee9affbcc5b9e79fc69422a06f32c1c
coderhormaz <hormazdaruwala86@gmail.com>
2025-07-08 13:17:45 +0530

    v7

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import AboutMe from './components/AboutMe'
 import Footer from './components/footer';
 import SplashCursor from './components/SplashCursor/SplashCursor';
 import './App.css'
-
-
+import DraggableCardDemo from './components/drag';
+import { TabsDemo } from './certificate';
 
 
 function App() {
@@ -13,8 +13,14 @@ function App() {
       <SplashCursor />
       <Hero />
       <AboutMe />
-  
-
+      <div className="about-header">
+        <h1>Project Showcase</h1>
+      </div>
+      <DraggableCardDemo />
+      <div className="about-header">
+        <h1>Certificate Showcase</h1>
+      </div>
+      <TabsDemo />
       <Footer />
     </>
   )
